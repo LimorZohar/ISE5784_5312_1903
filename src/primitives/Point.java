2fdb7a0adc3505a49dbb53c2b0282c250842f4c8
LimorZohar <limorz14123@gmail.com>
2024-05-07 18:38:52 +0300

    start stage1

---
+++ src/primitives/Point.java
@@ -0,0 +1,34 @@
+package primitives;
+
+public class Point {
+
+    public static final Point ZERO = new Point(Double3.ZERO);
+
+    final Double3 xyz;
+
+    public Point(double x, double y, double z) {
+        xyz = new Double3(x,y,z);
+    }
+    public Point(Double3 xyz) {
+        this.xyz = xyz;
+    }
+    public Vector subtract(Point p) {
+        return new Vector(xyz.subtract(p.xyz));
+    }
+    public Point add(Vector v1) {
+        return new Point(xyz.add(v1.xyz));
+    }
+    public double distanceSquared(Point p) {
+        Vector diff = subtract(p);
+
+        return(diff.xyz.d1 * diff.xyz.d1 +
+                diff.xyz.d2 * diff.xyz.d2 +
+                diff.xyz.d3 * diff.xyz.d3);
+    }
+    public double distance(Point p) {
+        return Math.sqrt(distanceSquared(p));
+    }
+
+
+
+}
