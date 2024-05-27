facaa023f03a9cf0e41f5b235196b594362ac9df
LimorZohar <limorz14123@gmail.com>
2024-05-27 08:52:28 +0300

    Sphere & Triangle tests

--- unittests/geometries/SphereTest.java
+++ unittests/geometries/SphereTest.java
@@ -1,6 +1,8 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,6 +11,12 @@ class SphereTest {
     final double DELTA = 0.0000001;
 
     @Test
-    void testGetNormal() {
+    public void testGetNormal() {
+        Sphere sphere = new Sphere(new Point(0,0,0), 1.0);
+        Point p = new Point(1, 0, 0);
+        Vector expect = new Vector(1, 0, 0).normalize();
+        // ============ Equivalence Partitions Tests ==============
+        // test point on the sphere
+        assertEquals(expect, sphere.getNormal(p), "getNormal() for Sphere did not return the expected normal");
     }
 }
--- unittests/geometries/TriangleTest.java
+++ unittests/geometries/TriangleTest.java
@@ -5,4 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class TriangleTest {
     final double DELTA = 0.000001;
 
+
+
 }
