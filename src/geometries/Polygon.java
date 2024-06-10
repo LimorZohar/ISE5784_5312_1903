2f3844a1b5811e7925e900bc6cecfd8a6c911325
LimorZohar <limorz14123@gmail.com>
2024-06-10 08:57:19 +0300

    stage 3 fix!

--- src/geometries/Polygon.java
+++ src/geometries/Polygon.java
@@ -95,40 +95,40 @@ public class Polygon implements Geometry {
 
     @Override
     public List<Point> findIntersections(Ray ray) {
-        List<Point>result=plane.findIntersections(ray);
+        List<Point> result = plane.findIntersections(ray);
 
-        if (result==null){
+        if (result == null) {
             return null;
         }
 
-        int numV=vertices.size();
+        int numV = vertices.size();
         Point p0 = ray.getHead();
         Vector v = ray.getDirection();
 
-        Vector v1=vertices.get(numV-1).subtract(p0);
-        Vector v2=vertices.get(0).subtract(p0);
+        Vector v1 = vertices.get(numV - 1).subtract(p0);
+        Vector v2 = vertices.get(0).subtract(p0);
 
-        Vector n= v1.crossProduct(v2).normalize();
-        double vn=v.dotProduct(n);
-        boolean positive= vn>0;
+        Vector n = v1.crossProduct(v2).normalize();
+        double vn = v.dotProduct(n);
+        boolean positive = vn > 0;
 
-        if(isZero(vn)){
+        if (isZero(vn)) {
             return null;
         }
 
-        for(int i=1; i<numV; ++i){
-            v1=v2;
-            v2=vertices.get(i).subtract(p0);
-            n=v1.crossProduct(v2).normalize();
-            vn=v.dotProduct(n);
+        for (int i = 1; i < numV; ++i) {
+            v1 = v2;
+            v2 = vertices.get(i).subtract(p0);
+            n = v1.crossProduct(v2).normalize();
+            vn = v.dotProduct(n);
 
             //no intersection
-            if(isZero(vn)){
+            if (isZero(vn)) {
                 return null;
             }
 
             //not the same sign
-            if(vn>0 != positive){
+            if (vn > 0 != positive) {
                 return null;
             }
         }
