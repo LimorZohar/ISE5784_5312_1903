1044241326e25c33c405c5745b4af34cdde649a0
Miriam <miriamlitov12321@gmail.com>
2024-06-18 12:43:52 +0300

    Add "public static AmbientLight None;"

--- src/lighting/AmbientLight.java
+++ src/lighting/AmbientLight.java
@@ -3,6 +3,8 @@ package lighting;
 import primitives.*;
 
 public class AmbientLight {
+    public static AmbientLight None;
+
     public AmbientLight(Color intensity, Double3 double3) {
         this.intensity = intensity;
     }
