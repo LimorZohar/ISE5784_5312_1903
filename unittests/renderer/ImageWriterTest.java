aa213a6871d740c593db4cc36e3e25b544199471
Miriam <miriamlitov12321@gmail.com>
2024-07-01 12:36:33 +0300

    order

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -3,26 +3,35 @@ package renderer;
 import org.junit.jupiter.api.Test;
 import primitives.Color;
 
-
+/**
+ * Unit tests for ImageWriter class.
+ */
 class ImageWriterTest {
 
     /**
      * The width of the view plane in pixels.
      */
     int nX = 800;
+
     /**
      * The height of the view plane in pixels.
      */
     int nY = 500;
+
     /**
      * The color yellow for the yellow square on the view plane.
      */
     Color yellowColor = new Color(255d, 255d, 0d);
+
     /**
      * The color red for the net on the view plane.
      */
     Color redColor = new Color(255d, 0d, 0d);
 
+    /**
+     * Test method for {@link renderer.ImageWriter#writeToImage()}.
+     * This method creates an image with a yellow background and a red grid.
+     */
     @Test
     void testWriteToImage() {
         ImageWriter imageWriter = new ImageWriter("yellow", nX, nY);
