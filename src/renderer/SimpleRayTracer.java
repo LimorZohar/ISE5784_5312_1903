18de7b88a8cf63b3d5483ea0d291e9a76e1367a1
Miriam <miriamlitov12321@gmail.com>
2024-07-03 13:32:42 +0300

    change SimpleRayTracer

--- src/renderer/SimpleRayTracer.java
+++ src/renderer/SimpleRayTracer.java
@@ -1,5 +1,6 @@
 package renderer;
 
+import geometries.Intersectable.GeoPoint;
 import primitives.Color;
 import primitives.Point;
 import primitives.Ray;
@@ -19,19 +20,23 @@ public class SimpleRayTracer extends RayTracerBase {
         super(scene);
     }
 
-    @Override
-    public Color traceRay(Ray ray) {
-        var intersections = this.scene.geometries.findIntersections(ray);
-        return intersections == null ? this.scene.background : calcColor(ray.findClosestPoint(intersections));
-    }
-
     /**
      * Get the color of an intersection point
      *
      * @param point point of intersection
      * @return Color of the intersection point
      */
-    private Color calcColor(Point point) {
-        return this.scene.ambientLight.getIntensity();
+    private Color calcColor(GeoPoint point) {
+        return this.scene.ambientLight.getIntensity().add(point.geometry.getEmission());
     }
+
+    @Override
+    public Color traceRay(Ray ray) {
+        var intersections = this.scene.geometries.findGeoIntersections(ray);
+        if (intersections == null) {
+            return scene.background;
+        }
+        return calcColor(ray.findClosestGeoPoint(intersections));
+    }
+
 }
